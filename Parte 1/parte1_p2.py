99575e314e9cccab501ff2fd2776712d57d1ab94
Gabriel Abarca <gababarca@hotmail.com>
2021-01-12 21:49:48 -0600

    [Add] Compose and Simple Classes

--- Parte 1/parte1_p2.py
+++ Parte 1/parte1_p2.py
@@ -1,1 +1,29 @@
-print('Hello World')
+class way:
+    def simpson(self) -> (str, int):
+        pass
+    def trapecio(self) -> (str, int):
+        pass
+    def boole(self) -> (str, int):
+        pass
+
+class simple(way):
+    def simpson(self) -> (str, int):
+        return '',0
+    def trapecio(self) -> (str, int):
+        return '',0
+    def boole(self) -> (str, int):
+        return '',0
+
+class compuesto(way):
+    def __init__(self, a = 0, b = 0):
+        self.a = a
+        self.b = b
+    def simpson(self) -> (str, int):
+        return '',0
+    def trapecio(self) -> (str, int):
+        return '',0
+    def boole(self) -> (str, int):
+        return '',0
+
+value: way = simple()
+print (value.simpson())
