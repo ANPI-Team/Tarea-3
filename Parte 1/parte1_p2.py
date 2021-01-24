27b033a28fdfc9e4cb9511c1e5eac9f834729eeb
BryanMM <bryanmasis87@gmail.com>
2021-01-24 16:39:41 -0600

    Add files via upload

--- Parte 1/parte1_p2.py
+++ Parte 1/parte1_p2.py
@@ -1,9 +1,7 @@
-from sympy import Symbol, S
-from sympy.calculus.util import continuous_domain
-from sympy.sets import Interval
-from sympy.parsing.sympy_parser import parse_expr
-from sympy.abc import x
-from sympy.utilities.lambdify import lambdify, implemented_function
+import numpy as np
+from sympy import *
+x = Symbol('x')
+
 
 class way:
     def simpson(self, func, a, b, puntos) -> (str, float):
@@ -14,10 +12,51 @@ class way:
         pass
 
 class simple(way):
-    def simpson(self, func, a, b, puntos = 0) -> (str, float):
-        return func,0.0
-    def trapecio(self, func, a, b, puntos = 0) -> (str, float):
-        return func,0.0
+    def simpson(self, func, a, b, puntos = 0) -> (float, float):
+        '''
+        Recibe como parámetro una función f en forma de string
+        En caso de necesitar funciones específicas (como sin, cos)
+        debe usar las propias de sympy, por ejemplo, e^x = exp(x) 
+        Caso de ejemplo: Simpson("exp(2*x)+2",1,2)
+        '''
+        fun = sympify(f)
+        h = (b-a)/2
+        x1 = (a+b)/2
+
+        result = ((h/3)*(fun.subs(x, a)+(4*fun.subs(x, x1))+fun.subs(x, b)))
+        aprox = result.evalf()
+        fourth_deriv = diff(f, x, x, x, x)
+        if(fourth_deriv.subs(x, a) > fourth_deriv.subs(x, b)):
+            e = ((h**5)/90)*abs(diff(fun, x, x, x, x).subs(x, b))
+            error = e.evalf()
+
+        else:
+            e = ((h**5)/90)*abs(diff(fun, x, x, x, x).subs(x, a))
+            error = e.evalf()
+        return aprox, error
+
+    def trapecio(self, func, a, b, puntos = 0) -> (float, float):
+        '''
+        Recibe como parámetro una función f en forma de string
+        En caso de necesitar funciones específicas (como sin, cos)
+        debe usar las propias de sympy, por ejemplo, e^x = exp(x) 
+        Caso de ejemplo: trapecio("exp(2*x)+2",1,2)
+        '''
+        fun = sympify(f)
+        h = b-a
+        result = ((h/2)*(fun.subs(x, a)+fun.subs(x, b)))
+        aprox = result.evalf()
+        second_deriv = diff(fun, x, x)
+
+        if(second_deriv.subs(x, a) > second_deriv.subs(x, b)):
+            e = (h**3/12)*abs(diff(fun, x, x).subs(x, b))
+            error = e.evalf()
+        else:
+            e = (h**3/12)*abs(diff(fun, x, x).subs(x, a))
+            error = e.evalf()
+
+        return aprox, error
+
     def boole(self, func, a, b, puntos = 0) -> (str, float):
         h = (b - a)/4
         f = lambdify(x, func)
@@ -32,11 +71,86 @@ class simple(way):
         return str(value),0.0
 
 class compuesto(way):
-    def simpson(self, func, a, b, puntos) -> (str, float):
-        return func,0.0
-    def trapecio(self, func, a, b, puntos) -> (str, float):
-        return func,0.0
-    def boole(self, func, a, b, puntos) -> (str, float):
+    def simpson(self, func, a, b, puntos) -> (float, float):
+        '''
+        Recibe como parámetro una función f en forma de string,
+        los valores "a" y "b" del intervalo donde se desea integrar
+        asi como la cantidad de puntos necesaria para la aproximacion,
+        en este caso, debe ser un número IMPAR de puntos.
+        En caso de necesitar funciones específicas (como sin, cos)
+        debe usar las propias de sympy, por ejemplo, e^x = exp(x) 
+        Caso de ejemplo: simpsonCompuesto("exp(2*x)+2",1,2,5)
+        '''
+        pairAprox = 0
+        impairAprox = 0
+        fun = sympify(f)
+        h = (b-a)/(N-1)
+
+        xVector = []
+        xVector.append(a)
+
+        for i in range(1, N):
+            xVector.append(a+i*h)
+        
+        for i in range(1, N-1):
+            pair = (i % 2 == 0)
+            if(pair):
+                print("is pair")
+                pairAprox = pairAprox + fun.subs(x, xVector[i])
+
+            else:
+                print("is impair")
+                impairAprox = impairAprox + fun.subs(x, xVector[i])
+
+        aprox = (h/3)*((fun.subs(x, xVector[0]))+2 *
+                    (pairAprox)+4*(impairAprox)+fun.subs(x, b))
+        aprox = aprox.evalf()
+
+        second_deriv = diff(fun, x, x)
+        if(second_deriv.subs(x, a) > second_deriv.subs(x, b)):
+            e = (((b-a)*(h**2))/12)*abs(diff(fun, x, x).subs(x, b))
+            error = e.evalf()
+
+        else:
+            e = (((b-a)*(h**2))/12)*abs(diff(fun, x, x).subs(x, a))
+            error = e.evalf()
+
+        return aprox, error
+
+    def trapecio(self, func, a, b, puntos) -> (int, float):
+        '''
+        Recibe como parámetro una función f en forma de string
+        En caso de necesitar funciones específicas (como sin, cos)
+        debe usar las propias de sympy, por ejemplo, e^x = exp(x) 
+        Caso de ejemplo: Simpson("exp(2*x)+2",1,2)
+        '''
+
+        fun = sympify(f)
+        h = (b-a)/(N-1)
+
+        xVector = []
+        xVector.append(a)
+        aprox = 0
+        for i in range(1, N):
+            xVector.append(a+i*h)
+
+        for i in range(0, N-1):
+            aprox = aprox + (fun.subs(x, xVector[i])+fun.subs(x, xVector[i+1]))
+
+        aprox = 0.5*aprox.evalf()
+
+        second_deriv = diff(fun, x, x)
+        if(second_deriv.subs(x, a) > second_deriv.subs(x, b)):
+            e = (((b-a)*(h**2))/12)*abs(diff(fun, x, x).subs(x, b))
+            error = e.evalf()
+
+        else:
+            e = (((b-a)*(h**2))/12)*abs(diff(fun, x, x).subs(x, a))
+            error = e.evalf()
+
+        return aprox, error
+
+    def cuadraturasGaussianas(self, func, a, b, puntos) -> (float, float):
         return func,0.0
 
 
