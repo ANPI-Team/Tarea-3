f64f1a8ee8d750cb232a42f2ae353cad4ca73f8b
Alejandra Castrillo <mcastrillo@estudiantec.cr>
2021-01-26 21:16:18 -0600

    Finished details in #4 #5

--- Parte 2/parte2_p2.m
+++ Parte 2/parte2_p2.m
@@ -1,5 +1,7 @@
 function funs = parte2_p2
   
+  warning('off', 'all');
+  
   % Formato para mostrar los datos 
   format short;
   
@@ -16,46 +18,58 @@ function funs = parte2_p2
   % Calculamos los puntos 'x' y 'y' con la función edo2
   [x, y] = parte2_p1.edo2(p, q, f, N, a, b, y0, yN)
   
-  % Calculamos los valores dados por el polinomio de lagrange
+  % Calculamos el polinomio de lagrange 
+  polSym = lagrangeP(x,y);
+  polCoefficients = sym2poly(polSym)
+  polFunction = function_handle(polSym);
+  
+  % Creamos los puntos en x para el polinomio
   polX   = a:((b-a)/(N*10)):b;
-  polY   = zeros(1, length(polX));
-
-  for i=1:length(polX)
-    polY(i) = lagPol(polX(i), x, y);
-  endfor
-
+  
+  % Calculamos los valores en y dados por el polinomio de lagrange
+  polY   = polFunction(polX);
+  
+  % Mostramos la grafica
   plot(polX, polY, 'b',x, y, 'or');
   
   % Cambiamos el formato para mostrar mayor precision en el error 
   format long;
-  error = errorLagrange(y0, polY(1))
+  yAproximado = polFunction(x);
+  errorRelativo = errorLagrange(y, yAproximado)  
+  display("\n\nJustificación: El error asociado al polinomio es de orden  de 10(-8) por lo que se puede considerar una buena aproximacion a la funcion.")
  
 endfunction
-  
-function y = lagPol(x, xVector, yVector)
-  
-  % Calculamos  los puntos por medio de la interpolacion de Lagrange 
-  N   = length(xVector);
-  sum = 0;
-  for i=1:N
-      p=1;
-      for j=1:N
-          if j~=i
-              p = p*(x-xVector(j))/(xVector(i)-xVector(j));
-          end
-      end
-      sum = sum + p*yVector(i);
-  end
-  y=sum;
-  
+
+function p = lagrangeP(x, y)
+  % Calculamos el polinomio de interpolacion
+  syms xsym;
+  % Inicialmos el polinomio en 0
+  p = 0;
+  for i=1:length(x)
+    % Calculamos el L(i)
+    L = 1;
+    for j=1:length(x)
+      if j~=i
+        L = L*((xsym-x(j))/(x(i)-x(j)));
+      endif
+    endfor
+    L = L*y(i);
+    % Sumamos el L(i) al polinomio
+    p = p+L;
+  endfor
 endfunction
 
 function error = errorLagrange(yOriginal, yAproximado)
   
-  % Calculamos el error relativo 
-  error = abs(yOriginal-yAproximado)/abs(yOriginal);
+  % Calculamos el promedio del error relativo de los puntos
+  error = 0;
+  for i=1:length(yOriginal)-1
+    error = error + abs(yOriginal(i)-yAproximado(i))/abs(yOriginal(i));
+  endfor
+  error = error/(length(yOriginal)-1);
   
 endfunction
+
 
 
 
